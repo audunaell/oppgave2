75b7730fd7741e09aa5dd814560dfcc6cf64d8c3
audunaell <62010741+audunaell@users.noreply.github.com>
2020-04-20 13:41:04 +0200

    the right ones

---
+++ obli2.js
@@ -0,0 +1,46 @@
+// controller
+
+function selected(barNo) {
+    if (nameOfSelectedBar == barNo) {
+        nameOfSelectedBar = null;
+    } else if (nameOfSelectedBar == null) {
+        nameOfSelectedBar = barNo;
+    }
+    show();
+}
+
+function removeBar() {
+    let index = nameOfSelectedBar - 1;
+    numbers.splice(index, 1);
+    nameOfSelectedBar = null;
+    show();
+}
+
+function addBar() {
+    //let newBar = document.getElementById('barValue').value;
+    let testValue = testInput(newValue);
+
+    if (testValue == true) {
+        numbers.push(newValue);
+        show();
+    }
+}
+
+function changeBarValue() {
+    let index = nameOfSelectedBar - 1;
+    //let selValue = document.getElementById('barValue').value;
+    let testValue = testInput(newValue);
+
+    if (testValue == true) {
+        numbers[index] = newValue;
+        show();
+    }
+}
+
+function testInput(inpValue) {
+    if (inpValue >= 1 && inpValue <= 10 && isNaN(inpValue) == false) {
+        return true;
+    }
+    alert("Has to be a number from 1-10");
+    return false;
+}
